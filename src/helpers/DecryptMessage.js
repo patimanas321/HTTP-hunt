6f8e8391f3d9334ad3d462482480a86a5af21d50
Manas Kumar Pati <patimanas321@yahoo.com>
2020-07-21 20:45:13 +0530

    Decrypt Message helper

--- src/helpers/DecryptMessage.js
+++ src/helpers/DecryptMessage.js
@@ -1,7 +1,33 @@
 function decryptMessage(encryptedMessage, key) {
-
+    return [...encryptedMessage].map(char => decryptChar(char, key)).join('');
 }
 
-function decryptChar(char) {
-    const charCode = char.charCodeAt(0);
+function decryptChar(char, key) {
+    const upperCaseStart = 65;
+    const upperCaseEnd = 90;
+    const lowerCaseStart = 97;
+    const lowerCaseEnd = 122;
+
+    let charCode = char.charCodeAt(0);
+    key = key % 26;
+
+    const isUpperCaseLetter = (upperCaseStart <= charCode && charCode <= upperCaseEnd);
+    const isLowerCaseLetter = (lowerCaseStart <= charCode && charCode <= lowerCaseEnd);
+    if (isUpperCaseLetter || isLowerCaseLetter) {
+        charCode = charCode - key;
+
+        // Handle rotation
+        if (isUpperCaseLetter && charCode < upperCaseStart) {
+            charCode = upperCaseEnd - (upperCaseStart - charCode) + 1;
+        }
+        if (isLowerCaseLetter && charCode < lowerCaseStart) {
+            charCode = lowerCaseEnd - (lowerCaseStart - charCode) + 1;
+        }
+
+        return String.fromCharCode(charCode);
+    }
+
+    return char;
 }
+
+module.exports = decryptMessage;
